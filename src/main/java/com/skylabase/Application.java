5ad0f28662ae36a18653511a9fb8037d144726f7
Awasum Yannick <awasum.yannick@skylabase.com>
2017-12-26 12:51:54 +0100

    Just run the sample app: next we clean up the code with spring

--- src/main/java/com/skylabase/Application.java
+++ src/main/java/com/skylabase/Application.java
@@ -1,9 +1,15 @@
 package com.skylabase;
 
+import com.skylabase.service.CustomerService;
+import com.skylabase.service.CustomerServiceImpl;
+
 public class Application {
 
 	public static void main(String[] args) {
 		
+		CustomerService customerService = new CustomerServiceImpl();
+		
+		System.out.println(customerService.findAll().get(0).getFirstName());
 
 	}
 
