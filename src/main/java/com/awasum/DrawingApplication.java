57e3c3183c595d9f259436e5beb3c91eb6abcdc0
Awasum Yannick <awasum@apache.org>
2019-05-16 14:15:25 +0100

    Using ApplicationContext and Property Initialization

--- src/main/java/com/awasum/DrawingApplication.java
+++ src/main/java/com/awasum/DrawingApplication.java
@@ -1,24 +1,21 @@
 package com.awasum;
 
-import com.awasum.service.CustomerService;
-import com.awasum.service.CustomerServiceImpl;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApplication {
 
 	public static void main(String[] args) {
-		
-		CustomerService customerService = new CustomerServiceImpl();
-		
-		System.out.println(customerService.findAll().get(0).getFirstName());
 
-		final Triangle triangle = new Triangle();
-		triangle.draw();
+//		final Triangle triangle = new Triangle();
+//		triangle.draw();
+//
+//		final BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+//		Triangle beanTriangle = (Triangle) beanFactory.getBean("triangle");
+//		beanTriangle.draw();
 
-		final BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle beanTriangle = (Triangle) beanFactory.getBean("triangle");
+		final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle beanTriangle = (Triangle) applicationContext.getBean("triangle");
 		beanTriangle.draw();
 	}
 
--- src/main/java/com/awasum/Triangle.java
+++ src/main/java/com/awasum/Triangle.java
@@ -2,8 +2,18 @@ package com.awasum;
 
 public class Triangle {
 
+  private String type;
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
   public void draw() {
-    System.out.println("Triangle Drawn");
+    System.out.println(this.getType() + " Triangle Drawn");
   }
 
 }
