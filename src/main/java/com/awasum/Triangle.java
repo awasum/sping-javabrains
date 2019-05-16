c19f0c1c92c9cacda887efa0e8e3db63d830c87a
Awasum Yannick <awasum@apache.org>
2019-05-16 14:30:58 +0100

    Constructor injection

--- src/main/java/com/awasum/Triangle.java
+++ src/main/java/com/awasum/Triangle.java
@@ -3,17 +3,31 @@ package com.awasum;
 public class Triangle {
 
   private String type;
+  private int height;
+
+  public Triangle(String type) {
+    this.type = type;
+  }
+
+  public Triangle(String type, int height) {
+    this.type = type;
+    this.height = height;
+  }
+
+  public int getHeight() {
+    return height;
+  }
 
   public String getType() {
     return type;
   }
 
-  public void setType(String type) {
-    this.type = type;
-  }
+//  public void setType(String type) {
+//    this.type = type;
+//  }
 
   public void draw() {
-    System.out.println(this.getType() + " Triangle Drawn");
+    System.out.println(this.getType() + " Triangle Drawn of Height " + this.getHeight());
   }
 
 }
